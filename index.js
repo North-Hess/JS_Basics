db7c5987076c68204b8bad5f3e588cf355aa1278
North Hess <north4h@gmail.com>
2023-04-13 14:40:17 -0400

    mosh tutorial finished

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-let name = 'North';
+let name = 'North'; // string literal
 console.log(name);
+
+// Camel notation
+// Case-sensitive
+// dynamic language
+
+let firstName = 'North';
+let lastName = 'Hess';
+
+const interestRate = 0.3; // number literal
+// interestRate = 1;
+console.log(interestRate)
+
+let isApproved = false; // boolean literal
+let isUndefined = undefined;
+let isNull = null;
+
+// object, similar to dict?
+let person = {
+    name: 'North',
+    age: 22
+};
+// dot notation
+person.name = 'Not North'
+// bracket notation
+person['name'] = 'Also not North';
+console.log(person);
+
+// array, indexed
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors);
+
+
+// functions
+function greet(name) {
+    // function body defines logic
+    console.log('Hello ' + name);
+}
+
+greet(firstName);
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
+console.log(square(2));
